15364f66d34d403a96476b9870dbcabd1a2b35b2
Nayan Jain <nayanjain1994@gmail.com>
2021-11-27 14:08:34 -0500

    Chart added to homepage

--- DBMS_Project/Angular/covid-airline-analysis/src/app/chart/chart.component.ts
+++ DBMS_Project/Angular/covid-airline-analysis/src/app/chart/chart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit,  Inject, NgZone, PLATFORM_ID} from '@angular/core';
-// import { Component, Inject, NgZone, PLATFORM_ID } from '@angular/core';
+import { OracleQuery1ServiceService } from '../oracle-query1-service.service';
 import { isPlatformBrowser } from '@angular/common';
 import * as am4core from '@amcharts/amcharts4/core';
 import * as am4maps from '@amcharts/amcharts4/maps';
@@ -16,13 +16,11 @@ import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 })
 export class ChartComponent implements OnInit {
 
+  resultData: any;
+  resultDatas: any;
   private chart: am4charts.XYChart;
 
-  constructor(@Inject(PLATFORM_ID) private platformId:any, private zone: NgZone) {}
-
-
-  ngOnInit(): void {
-  }
+  constructor(@Inject(PLATFORM_ID) private platformId:any, private zone: NgZone, private query1dta : OracleQuery1ServiceService) {}
 
   browserOnly(f: () => void) {
     if (isPlatformBrowser(this.platformId)) {
@@ -32,13 +30,20 @@ export class ChartComponent implements OnInit {
     }
   }
 
+   ngOnInit(): void {
 
-  ngAfterViewInit() {
-    // Chart code goes in here
-    this.browserOnly(() => {
+    this.query1dta.getCharts().subscribe(
+      res => {
+        this.resultData = res;
+        this.resultDatas = []
+        for (let item of this.resultData['rows']) {
+        this.resultDatas.push({
+            id:    'US-' + item[0],
+            value:  item[1]
+        });
+          }
+        this.browserOnly(() => {
       am4core.useTheme(am4themes_animated);
-
-//       let chart = am4core.create("chartdiv", am4charts.XYChart);
 
       let chart = am4core.create("chartdiv", am4maps.MapChart);
       chart.geodata = am4geodata_usaLow;
@@ -51,209 +56,7 @@ export class ChartComponent implements OnInit {
         max: chart.colors.getIndex(1).brighten(-0.3)
       });
       polygonSeries.useGeodata = true;
-
-      polygonSeries.data = [
-      {
-        id: "US-AL",
-        value: 4447100
-      },
-      {
-        id: "US-AK",
-        value: 626932
-      },
-      {
-        id: "US-AZ",
-        value: 5130632
-      },
-      {
-        id: "US-AR",
-        value: 2673400
-      },
-      {
-        id: "US-CA",
-        value: 33871648
-      },
-      {
-        id: "US-CO",
-        value: 4301261
-      },
-      {
-        id: "US-CT",
-        value: 3405565
-      },
-      {
-        id: "US-DE",
-        value: 783600
-      },
-      {
-        id: "US-FL",
-        value: 15982378
-      },
-      {
-        id: "US-GA",
-        value: 8186453
-      },
-      {
-        id: "US-HI",
-        value: 1211537
-      },
-      {
-        id: "US-ID",
-        value: 1293953
-      },
-      {
-        id: "US-IL",
-        value: 12419293
-      },
-      {
-        id: "US-IN",
-        value: 6080485
-      },
-      {
-        id: "US-IA",
-        value: 2926324
-      },
-      {
-        id: "US-KS",
-        value: 2688418
-      },
-      {
-        id: "US-KY",
-        value: 4041769
-      },
-      {
-        id: "US-LA",
-        value: 4468976
-      },
-      {
-        id: "US-ME",
-        value: 1274923
-      },
-      {
-        id: "US-MD",
-        value: 5296486
-      },
-      {
-        id: "US-MA",
-        value: 6349097
-      },
-      {
-        id: "US-MI",
-        value: 9938444
-      },
-      {
-        id: "US-MN",
-        value: 4919479
-      },
-      {
-        id: "US-MS",
-        value: 2844658
-      },
-      {
-        id: "US-MO",
-        value: 5595211
-      },
-      {
-        id: "US-MT",
-        value: 902195
-      },
-      {
-        id: "US-NE",
-        value: 1711263
-      },
-      {
-        id: "US-NV",
-        value: 1998257
-      },
-      {
-        id: "US-NH",
-        value: 1235786
-      },
-      {
-        id: "US-NJ",
-        value: 8414350
-      },
-      {
-        id: "US-NM",
-        value: 1819046
-      },
-      {
-        id: "US-NY",
-        value: 18976457
-      },
-      {
-        id: "US-NC",
-        value: 8049313
-      },
-      {
-        id: "US-ND",
-        value: 642200
-      },
-      {
-        id: "US-OH",
-        value: 11353140
-      },
-      {
-        id: "US-OK",
-        value: 3450654
-      },
-      {
-        id: "US-OR",
-        value: 3421399
-      },
-      {
-        id: "US-PA",
-        value: 12281054
-      },
-      {
-        id: "US-RI",
-        value: 1048319
-      },
-      {
-        id: "US-SC",
-        value: 4012012
-      },
-      {
-        id: "US-SD",
-        value: 754844
-      },
-      {
-        id: "US-TN",
-        value: 5689283
-      },
-      {
-        id: "US-TX",
-        value: 20851820
-      },
-      {
-        id: "US-UT",
-        value: 2233169
-      },
-      {
-        id: "US-VT",
-        value: 608827
-      },
-      {
-        id: "US-VA",
-        value: 7078515
-      },
-      {
-        id: "US-WA",
-        value: 5894121
-      },
-      {
-        id: "US-WV",
-        value: 1808344
-      },
-      {
-        id: "US-WI",
-        value: 5363675
-      },
-      {
-        id: "US-WY",
-        value: 493782
-      }
-    ];
+      polygonSeries.data = this.resultDatas;
 
     // Set up heat legend
     let heatLegend = chart.createChild(am4maps.HeatLegend);
@@ -268,10 +71,10 @@ export class ChartComponent implements OnInit {
     // Set up custom heat map legend labels using axis ranges
     let minRange = heatLegend.valueAxis.axisRanges.create();
     minRange.value = heatLegend.minValue;
-    minRange.label.text = "Little";
+    minRange.label.text = "Lowest";
     let maxRange = heatLegend.valueAxis.axisRanges.create();
     maxRange.value = heatLegend.maxValue;
-    maxRange.label.text = "A lot!";
+    maxRange.label.text = "Highest";
 
     // Blank out internal heat legend value axis labels
     heatLegend.valueAxis.renderer.labels.template.adapter.add("text", function(labelText) {
@@ -287,6 +90,7 @@ export class ChartComponent implements OnInit {
     let hs = polygonTemplate.states.create("hover");
     hs.properties.fill = am4core.color("#3c5bdc");
 
+    });
     });
   }
 
